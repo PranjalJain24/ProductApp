80978675da447e7c4d1c80c602d851e78392f6ba
Pranjal Jain <pranjal.jain@SLS.ads.valuelabs.net>
2019-06-11 18:28:27 +0530

    Raw html, Django form and widgets

--- products/forms.py
+++ products/forms.py
@@ -10,3 +10,19 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+        
+class RawProductForm(forms.Form):
+    title= forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder":"Your Title"}))
+    description= forms.CharField(
+                        required=False,
+                        widget=forms.Textarea(
+                            attrs={
+                                "plaeholder" : "Your description"                            
+                                "class" : "new_class_name two",
+                                "id":"my-id-for-txtarea ",
+                                "rows":10,
+                                "column":20
+                            }
+                        )
+                    )
+    price = forms.DecimalField(initial=999.99 )
--- products/views.py
+++ products/views.py
@@ -1,19 +1,45 @@
 from django.shortcuts import render
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 # Create your views here.
 
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form  =ProductForm()
-        
-    context= {
-        'form' : form
+    my_form = RawProductForm()
+    if request.method == 'POST':
+        my_form = RawProductForm(request.POST) #validation
+        if my_form.is_valid():
+            print(my_form.cleaned_data)
+            Product.objects.create(**my_form.cleaned_data)
+        else:
+            print(my_form.errors)
+    context = {
+        'form': my_form
     }
-    
     return render(request, 'products/product_create.html', context)
+  
+
+#RAW HTML FORM
+# def product_create_view(request):
+#     print(request.GET)
+#     print(request.POST)
+#     if request.method == 'POST':
+#         my_new_title = request.POST.get('title')
+#         print(my_new_title)
+    
+#     context = {}
+#     return render(request, 'products/product_create.html', context)
+  
+#Simple
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form  =ProductForm()
+        
+#     context= {
+#         'form' : form
+#     } 
+#     return render(request, 'products/product_create.html', context)
   
   
 def product_detail_view(request):
