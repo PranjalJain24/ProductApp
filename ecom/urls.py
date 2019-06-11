938996bf97a2dd3508cf3db418ffc30c92877f90
Pranjal Jain <pranjal.jain@SLS.ads.valuelabs.net>
2019-06-11 12:17:01 +0530

    Render data from DB

--- ecom/urls.py
+++ ecom/urls.py
@@ -15,14 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages import views
+#from pages import views
+#from products import views
 from pages.views import home_view,about_view,social_view,contact_view
+from products.views import product_detail_view
 
 urlpatterns = [
-    path("",views.home_view),
-    path("home",views.home_view),
-    path("contact",views.contact_view),
-    path("about",views.about_view),
-    path("social",views.social_view),
+    path("",home_view),
+    path("home",home_view),
+    path("contact",contact_view),
+    path("about",about_view),
+    path("social",social_view),
+    path("product",product_detail_view),
     path('admin/', admin.site.urls),
+    
 ]
